38f61f78a69d1bf7d68d14c3edb0bcdc4b7c2401
Mateusz Zieba <mateusz.zieba@onet.pl>
2018-03-14 13:24:28 +0100

    add comments to code and change variable declarations to es6 standard

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
+/*
+The function below shows shape of card back
+and adds the revealing mechanics
+*/
 function turnCard() {
-    var card = this.children[0];
-    card.style.display="block";
-    cardsArray.push(card);
-    if (cardsArray.length == 2) {
+	if (flag == true) { // only if flag == true, carry on
+	    const card = this.children[0]; // assign children element of clicked div to variable
+	    card.style.display="block"; // change style of display to show the shape
+	    cardsArray.push(card); // and push it "cardsArray" 
+	    this.removeEventListener("click", turnCard); // remove event listener to prevent next clicks
 
-    	setTimeout(checkReversals, 1550);
-    }
+	    if (cardsArray.length == 2) { // if 2 cards were clicked
+	    	flag = false; // change flag to false to prevent next clicks on whole game board until animation end
+	    	setTimeout(checkReversals, 1500); // call next function, with delay 1500ms
+	    }
+	}
 }
 
+/*
+The function below checks if two
+consecutive clicks show the same shape
+*/
 function checkReversals() {
-	if (cardsArray[0].className == cardsArray[1].className) {
-		console.log("well done");
-		cardsArray = [];
-		points += 1;
-		if (points == 8) {
+	if (cardsArray[0].className == cardsArray[1].className) { // if card backs were the same:
+		cardsArray = []; // clear array
+		points += 1; // add 1 point
+		if (points == 8) { // if points == 8 you won ;)
 			alert("WELL DONE");
-		}
+		}	
 	}
-	else {
-		cardsArray[0].style.display="none";
+	else { // if card back weren't the same:
+		cardsArray[0].style.display="none"; // hide cards
 		cardsArray[1].style.display="none";
+		
+		/*
+		add event listeners again, because it 
+		were removed while click event occured	
+		*/
+		cardsArray[0].parentElement.addEventListener("click", turnCard);
+		cardsArray[1].parentElement.addEventListener("click", turnCard);
 		cardsArray = [];
 	}
+	flag = true; // set flag to true, so that you can continue use "turnCard" function
 }
 
-var cards = document.querySelectorAll(".card");  
 
+/*
+Code below assign all html elements with class 
+"card" to cards variable, then add 
+Event Listener to each of them
+*/
+const cards = document.querySelectorAll(".card");  
 cards.forEach(function(card) {
 	card.addEventListener("click", turnCard);
 });
 
-var cardsArray = [];
-var points = 0;
+
+let cardsArray = []; // Declare empty array for memorize cards
+let points = 0; // Declare points variable to count points
+let flag = true; // Declare flag to check if the function can be performed
