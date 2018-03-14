25b6b8952e83795534a928c73ce69fe24297c9b6
Mateusz Zieba <mateusz.zieba@onet.pl>
2018-03-14 14:57:56 +0100

    add shuffle function

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function turnCard() {
 
 	    if (cardsArray.length == 2) { // if 2 cards were clicked
 	    	flag = false; // change flag to false to prevent next clicks on whole game board until animation end
-	    	setTimeout(checkReversals, 1500); // call next function, with delay 1500ms
+	    	setTimeout(checkReversals, 155); // call next function, with delay 1500ms
 	    }
 	}
 }
@@ -43,13 +43,28 @@ function checkReversals() {
 	flag = true; // set flag to true, so that you can continue use "turnCard" function
 }
 
+/*
+Function shuffle cards in random order
+Take each of the element with class "card__back"
+Splice method remove and return one class from classes array
+and add it to first element with class "card__back", and so on...
+*/
+function shuffle() {
+	// Create an array of classes with shapes
+	let classes = ["icon-heart", "icon-camera", "icon-ambulance", "icon-thumbs-up-alt", "icon-paper-plane", "icon-binoculars", "icon-plug", "icon-user-secret", 
+	"icon-heart", "icon-camera", "icon-ambulance", "icon-thumbs-up-alt", "icon-paper-plane", "icon-binoculars", "icon-plug", "icon-user-secret"];
+
+	card_backs.forEach(function(cb) {
+		cb.classList.add(classes.splice(Math.floor((Math.random()*classes.length)), 1 ));
+	});
+}
 
 /*
 Code below assign all html elements with class 
 "card" to cards variable, then add 
 Event Listener to each of them
 */
-const cards = document.querySelectorAll(".card");  
+const cards = document.querySelectorAll(".card");
 cards.forEach(function(card) {
 	card.addEventListener("click", turnCard);
 });
@@ -58,3 +73,8 @@ cards.forEach(function(card) {
 let cardsArray = []; // Declare empty array for memorize cards
 let points = 0; // Declare points variable to count points
 let flag = true; // Declare flag to check if the function can be performed
+
+// Grab all elements with card__back class
+const card_backs = document.querySelectorAll(".card__back");
+shuffle();
+
